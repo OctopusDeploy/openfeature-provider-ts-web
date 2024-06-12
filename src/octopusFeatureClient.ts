410ac8019a6d6c646590b62db0930f8db9797797
Andrew Best <andrew.best@octopus.com>
2024-06-12 12:50:13 +0930

    Initial implementation

---
+++ src/octopusFeatureClient.ts
@@ -0,0 +1,82 @@
+import axios, { AxiosRequestConfig } from "axios";
+import { FeatureToggleEvaluation, FeatureToggles, OctopusFeatureContext } from "./octopusFeatureContext";
+import { OctopusFeatureConfiguration } from "./octopusFeatureProvider";
+
+export class OctopusFeatureClient {
+    private context: OctopusFeatureContext;
+    private configuration: OctopusFeatureConfiguration;
+
+    constructor(configuration: OctopusFeatureConfiguration) {
+        this.context = new OctopusFeatureContext({ evaluations: [], contentHash: "" });
+        this.configuration = configuration;
+    }
+
+    async getEvaluationContext(): Promise<OctopusFeatureContext> {
+
+        // If the features have not changed, return the feature set
+
+        if (!await this.haveFeaturesChanged()) {
+            return this.context;
+        }
+
+        const manifest = await this.getFeatureManifest();
+
+        if (manifest === undefined) {
+            return this.context;
+        }
+
+        this.context = new OctopusFeatureContext(manifest);
+
+        return this.context;
+
+    }
+
+    async haveFeaturesChanged(): Promise<boolean> {
+        if (this.context.toggles.contentHash === "") return true;
+
+        const config: AxiosRequestConfig = {
+            url: `api/featuretoggles/${this.configuration.clientIdentifier}/check`,
+            maxContentLength: Infinity,
+            maxBodyLength: Infinity,
+            method: "GET",
+            responseType: "json",
+        };
+
+        // TODO: Validate this behaviour if the array is null
+        const response = await axios.request<Uint8Array>(config);
+
+        if (!response.data.length) {
+            return true;
+        }
+
+        var decoder = new TextDecoder("utf8");
+        var decodedContentHash = btoa(decoder.decode(response.data));
+
+        const haveFeaturesChanged = this.context.toggles.contentHash !== decodedContentHash;
+
+        return haveFeaturesChanged;
+    }
+
+    async getFeatureManifest(): Promise<FeatureToggles | undefined> {
+
+        const config: AxiosRequestConfig = {
+            url: `api/featuretoggles/v2/${this.configuration.clientIdentifier}`,
+            maxContentLength: Infinity,
+            maxBodyLength: Infinity,
+            method: "GET",
+            responseType: "json",
+        };
+
+        const response = await axios.request<FeatureToggleEvaluation[]>(config);
+
+        if (response.status == 404) {
+            return undefined;
+        }
+
+        if (!response.headers["ContentHash"]) {
+            return undefined;
+        }
+
+        return { evaluations: response.data, contentHash: response.headers["ContentHash"] };
+    }
+}
---
+++ src/octopusFeatureProvider.ts
@@ -0,0 +1,56 @@
+import { EvaluationContext, JsonValue, Provider, ResolutionDetails } from "@openfeature/web-sdk";
+import { OctopusFeatureClient } from "./octopusFeatureClient";
+import { OctopusFeatureContext } from "./octopusFeatureContext";
+
+export interface OctopusFeatureConfiguration {
+    serverUri: string;
+    clientIdentifier: string;
+    cacheDurationInMinutes: number;
+}
+
+export class OctopusFeatureProvider implements Provider {
+    private client: OctopusFeatureClient;
+    private evaluationContext: OctopusFeatureContext;
+    private context: EvaluationContext;
+
+    constructor(configuration: OctopusFeatureConfiguration) {
+        this.client = new OctopusFeatureClient(configuration);
+        this.evaluationContext = new OctopusFeatureContext({ evaluations: [], contentHash: "" });
+        this.context = {};
+    }
+
+    metadata = {
+        name: OctopusFeatureProvider.name,
+    };
+
+    readonly runsOn = "client";
+
+    hooks = [];
+
+    async onContextChange(oldContext: EvaluationContext, newContext: EvaluationContext): Promise<void> {
+        // code to run to reconcile the providers state with the newly updated context
+        this.context = newContext;
+        this.evaluationContext = await this.client.getEvaluationContext();
+    }
+
+    resolveBooleanEvaluation(flagKey: string, defaultValue: boolean): ResolutionDetails<boolean> {
+
+        const isFeatureEnabled = this.evaluationContext.evaluate(flagKey, defaultValue, this.context);
+
+        return isFeatureEnabled;
+    }
+
+    resolveStringEvaluation(flagKey: string, defaultValue: string): ResolutionDetails<string> {
+        throw new Error("Octopus Features only support boolean toggles.");
+    }
+
+    resolveNumberEvaluation(flagKey: string, defaultValue: number): ResolutionDetails<number> {
+        throw new Error("Octopus Features only support boolean toggles.");
+    }
+
+    resolveObjectEvaluation<U extends JsonValue>(flagKey: string, defaultValue: U): ResolutionDetails<U> {
+        throw new Error("Octopus Features only support boolean toggles.");
+    }
+
+
+}
