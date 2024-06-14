f8dbc922e65a3ca2cd93308afa10669a09485593
Andrew Best <andrew.best@octopus.com>
2024-06-14 13:07:38 +0930

    Remove hash checking from frontend client

--- src/octopusFeatureClient.ts
+++ src/octopusFeatureClient.ts
@@ -4,13 +4,11 @@ import { FeatureToggleEvaluation, FeatureToggles, OctopusFeatureContext } from "
 import { OctopusFeatureConfiguration } from "./octopusFeatureProvider";
 
 export class OctopusFeatureClient {
-    private context: OctopusFeatureContext;
-    private clientIdentifier: string;
-    private serverUri: string;
-    private axiosInstance: AxiosInstance;
+    private readonly clientIdentifier: string;
+    private readonly serverUri: string;
+    private readonly axiosInstance: AxiosInstance;
 
     constructor(configuration: OctopusFeatureConfiguration) {
-        this.context = new OctopusFeatureContext({ evaluations: [], contentHash: "" });
         this.clientIdentifier = configuration.clientIdentifier;
         this.serverUri = configuration.serverUri ? configuration.serverUri.replace(/\/$/, "") : "https://features.octopus.com";
         this.axiosInstance = axios.create();
@@ -18,47 +16,13 @@ export class OctopusFeatureClient {
     }
 
     async getEvaluationContext(): Promise<OctopusFeatureContext> {
-        // If the features have not changed, return the feature set
-
-        if (!(await this.haveFeaturesChanged())) {
-            return this.context;
-        }
-
         const manifest = await this.getFeatureManifest();
 
         if (manifest === undefined) {
-            return this.context;
+            return new OctopusFeatureContext({ evaluations: [], contentHash: "" });
         }
 
-        this.context = new OctopusFeatureContext(manifest);
-
-        return this.context;
-    }
-
-    async haveFeaturesChanged(): Promise<boolean> {
-        if (this.context.toggles.contentHash === "") return true;
-
-        const config: AxiosRequestConfig = {
-            url: `${this.serverUri}/api/featuretoggles/${this.clientIdentifier}/check`,
-            maxContentLength: Infinity,
-            maxBodyLength: Infinity,
-            method: "GET",
-            responseType: "json",
-        };
-
-        // TODO: Validate this behaviour if the array is null
-        const response = await this.axiosInstance.request<Uint8Array>(config);
-
-        if (!response.data.length) {
-            return true;
-        }
-
-        const decoder = new TextDecoder("utf8");
-        const decodedContentHash = btoa(decoder.decode(response.data));
-
-        const haveFeaturesChanged = this.context.toggles.contentHash !== decodedContentHash;
-
-        return haveFeaturesChanged;
+        return new OctopusFeatureContext(manifest);
     }
 
     async getFeatureManifest(): Promise<FeatureToggles | undefined> {
