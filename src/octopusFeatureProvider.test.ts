52e610d8406446763a120156ad7bf65038aa2ab4
Andrew Best <andrew.best@octopus.com>
2025-06-04 16:31:22 +0930

    Updates dependencies (#17)
    fix: updates dependencies

--- src/octopusFeatureProvider.test.ts
+++ src/octopusFeatureProvider.test.ts
@@ -1,12 +1,24 @@
 import { OctopusFeatureProvider } from "./octopusFeatureProvider";
 import { OpenFeature } from "@openfeature/web-sdk";
 
-// TODO: Set up the needful for this to be a stable integration test that can be run on each build.
 describe.skip("octopusFeatureProvider", () => {
+    // Stub out local storage for this particular test
+    global.localStorage = {
+        getItem: jest.fn(),
+        setItem: jest.fn(),
+        clear: jest.fn(),
+        length: 0,
+        key: jest.fn(),
+        removeItem: jest.fn(),
+    };
+
     test("use this to verify that the provider is happy end to end", async () => {
-        const client = new OctopusFeatureProvider({ clientIdentifier: "TODO" });
+        const client = new OctopusFeatureProvider({
+            clientIdentifier: "TODO",
+        });
+
         await OpenFeature.setProviderAndWait(client);
-        await OpenFeature.setContext({ userid: "abc" });
+        await OpenFeature.setContext({ username: "admin" });
         const result = OpenFeature.getClient().getBooleanValue("heartbeat", false, {});
         expect(result).toBe(true);
     });
