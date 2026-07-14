b0d8dcaed025326c4b6e5c9ba456a9f6cac50176
Yang Guo <yangguoif@gmail.com>
2026-07-14 15:02:21 +1200

    tweaks

--- renovate-config.js
+++ renovate-config.js
@@ -12,21 +12,26 @@ module.exports = {
 
   // Raise PRs for known vulnerabilities (replaces Dependabot security alerts).
   osvVulnerabilityAlerts: true,
-
-  // We don't want to pick up third party dependencies the instant they arrive,
-  // to give some time for any issues to be discovered, but not wait too long either.
   minimumReleaseAge: "2 days",
 
   // Match the conventional-commit style used by release-please in this repo.
+  // Runtime dependencies default to `fix` so they trigger a patch release.
   semanticCommits: "enabled",
   semanticCommitType: "fix",
   semanticCommitScope: "deps",
 
   extends: [
     "config:recommended",
-    ":automergeMinor",
     "helpers:pinGitHubActionDigests", // Pin all GitHub Actions to a commit SHA.
   ],
 
   enabledManagers: ["npm", "github-actions"],
+
+  packageRules: [
+    {
+      // Dev-time dependencies don't ship in the published package.
+      matchDepTypes: ["devDependencies"],
+      semanticCommitType: "chore",
+    },
+  ],
 };
